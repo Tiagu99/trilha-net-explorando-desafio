21565518ef1115f682fd1032a5083d20c2a67483
Tiago Moreira <tiago.joke@yahoo.com.br>
2023-10-05 13:30:54 -0300

    feat(extra): starts creating a menu

--- Program.cs
+++ Program.cs
@@ -3,6 +3,29 @@ using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
+int opcao = 0;
+
+do
+{
+    Console.WriteLine("Bem vindo ao sistema de cadastro de reserva");
+    Console.WriteLine("digite a opção desejada: \n 1 - Cadastrar nova reserva\n 2 - Listar reservas existentes\n 0 - Encerrar programa");
+    switch(opcao)
+    {
+        case 1:
+        break;
+        case 2:
+        break;
+        default:
+        break;
+    }
+}while(opcao != 0);
+
+
+
+
+
+
+
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
